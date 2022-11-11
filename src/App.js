7278fc8bed443657f67ab12562fa73b159b7d7fb
Tove Adamsson <toveadamsson@Toves-MBP.home>
2022-11-11 12:26:12 +0100

    updating styles

--- src/App.js
+++ src/App.js
@@ -1,67 +1,71 @@
-import React, { useEffect, useState } from "react";
-import Recipe from "./Recipe.js";
-import "./App.css";
+import React, { useEffect, useState } from 'react'
+import Recipe from './Recipe.js'
+import './App.css'
 
-function App() {
-  const YOUR_APP_ID = "35ef56dd";
-  const YOUR_APP_KEY = "97b46772b82b90f479602f2f416f3f21";
+function App () {
+  const YOUR_APP_ID = '35ef56dd'
+  const YOUR_APP_KEY = '97b46772b82b90f479602f2f416f3f21'
 
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
 
   const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
-    ); //gathering data from the api
-    const data = await response.json();
-    // console.log(data.hits);
-    setRecipes(data.hits);
-  };
-
-  console.log(recipes);
+    )
+    const data = await response.json()
+    setRecipes(data.hits)
+    // console.log(data.hits.recipe,"-----");
+  }
 
   useEffect(() => {
-    getRecipes();
-  }, [query]);
+    getRecipes()
+  }, [query])
 
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-    // console.log(search);
-  };
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
 
-  const getSearch = (e) => {
-    e.preventDefault();
-    setQuery(search); //here we get the finished text that the user writes in the input, instead for updating after every action the user types
-    setSearch("");
-  };
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search) //here we get the finished text that the user writes in the input, instead for updating after every action the user types
+    setSearch('')
+  }
+
+  recipes.forEach(e => {
+    console.log(e)
+  })
 
   return (
-    <div className="App">
-      <form onSubmit={getSearch} className="search-form">
+    <div className='App'>
+      <form onSubmit={getSearch} className='search-form'>
         <input
-          className="search-field"
-          type="text"
+          className='search-field'
+          type='text'
           value={search}
           onChange={updateSearch}
         />
-        <button className="search-button" type="submit">
+        <button className='search-button' type='submit'>
           Search
         </button>
       </form>
-      <div className="recipes-container">
-        {recipes.map((recipe) => (
+      <div className='recipes-container'>
+        {recipes.map((recipe,idx) => (
           <Recipe
-            key={recipe.recipe.label}
+            key={idx}
+            allRecipeLabel={recipe.recipe}
             title={recipe.recipe.label}
             calories={recipe.recipe.calories}
             image={recipe.recipe.image}
             ingredients={recipe.recipe.ingredients}
+            healthLabels={recipe.recipe.healthLabels}
+            cuisineType={recipe.recipe.cuisineType}
           />
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,21 +1,25 @@
-import React from "react";
-import style from "./recipe.module.css";
-const Recipe = ({ title, image, ingredients }) => {
-
-
+import React from 'react'
+import style from './recipe.module.css'
+const Recipe = ({ title, image, ingredients, healthLabels,cuisineType }) => {
 
   return (
     <div className={style.recipe}>
-      <h1 >{title}</h1>
-{/* hello */}
+      <h1>{title}</h1>
+    
+      <div>
+        <img className={style.img} src={image} alt='' />
+      </div>
       <ol className={style.list}>
-        {ingredients.map((ingredient) => (
-          <li>{ingredient.text}</li>
+        {ingredients.map((ingredient,idx) => (
+
+          <div key={idx}>
+            <li>{ingredient.text}</li>
+
+          </div>
         ))}
       </ol>
-      <img className={style.img} src={image} alt="" />
     </div>
-  );
-};
+  )
+}
 
-export default Recipe;
+export default Recipe
